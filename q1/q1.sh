d392f8399412b2981844e3e28563fd4a39a54b94
Ian Cleasby <Inc2En@hotmail.com>
2017-08-11 15:58:49 +1000

    Update q1.sh

--- q1/q1.sh
+++ q1/q1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# finds the directory name at any level
+
+# finds the directory name at any subdirectory level
 # grep -r recursively finds all the files, excluding directories
 # wc -w counts all the words
+
+
 find . -name $1 | xargs -I subdir grep -r '' subdir | wc -w
