4d3f49216cdbac0cc90bc4356c87913da7b617fe
Ian Cleasby <icle6423@uni.sydney.edu.au>
2017-08-12 21:50:41 +1000

    change q1

--- q1/q1.sh
+++ q1/q1.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# finds the directory name at any subdirectory level
-# grep -r recursively finds all the files, excluding directories
-# wc -w counts all the words
+# Argument is a sub-directory name
 
+# finds -name : the directory name at any subdirectory level
+# find -type f : recursively finds all the files, excluding directories
+# wc -w  : counts all the words
 
-find . -name $1 | xargs -I subdir grep -r '' subdir | wc -w
+find . -name $1 | xargs -I dir find dir -type f | xargs wc -w
+
